ca3c64bef0064470fb7b1a39230d0fefaee62778
cono152 <o9o97444159@yahoo.co.jp>
2024-10-04 15:27:47 +0900

    空のコミット

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -40,7 +40,6 @@ class CalendarsController < ApplicationController
 
         days = { month: (@todays_date + x).month, date: (@todays_date+x).day, wday: wday, plans: today_plans}
       @week_days.push(days)
-
     end
   end
 end
