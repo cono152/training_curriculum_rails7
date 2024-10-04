dd841150feb17ac7babe513503a4becaa3d61d16
cono152 <o9o97444159@yahoo.co.jp>
2024-10-04 14:31:54 +0900

    Revert "Merge branch 'master' of https://github.com/cono152/training_curriculum_rails7"
    This reverts commit fbc02fbd06961f177b492dcaa5fe16a1181854f3, reversing
    changes made to 8ba1983b09102d3b4134311a4564bce9d4b71525.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,7 +15,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:plan).permit(:data, :plan)
+    params.require(:calendars).permit(:date, :plan)
   end
 
   def get_week
@@ -34,8 +34,11 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
+      
+      wday_num = (Date.today + x).wday
+      wday = wdays[wday_num]
 
-      days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :wday => wdays[x], :plans => today_plans}
+        days = { month: (@todays_date + x).month, date: (@todays_date+x).day, wday: wday, plans: today_plans}
       @week_days.push(days)
 
     end
